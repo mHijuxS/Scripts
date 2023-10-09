a0071bbf8c3f6eec1930453b9ba57d6a87a3e3f8
mHiluxS <108304662+mHijuxS@users.noreply.github.com>
2023-10-09 19:54:17 -0300

    UpdtPS

--- Powershell/PingSweeper.ps1
+++ Powershell/PingSweeper.ps1
@@ -1,12 +1,14 @@
 ﻿param($p1)
+
 if (!$p1){
-    echo "Ping Sweeper"
-    echo "Exemplo de Uso .\script.ps1 192.168.0"
+    Write-Output "Ping Sweeper By mHiluxS"
+    Write-Output "Exemplo de Uso .\script.ps1 192.168.0"
+
 }else{
     foreach ($ip in 1..254){
     try{
         $resp = ping -n 1 -w 1 "$p1.$ip" | Select-String "bytes=32"
         $resp.Line.split(' ')[2] -replace ":",""
+
 }catch{}
-}
-}
+}}
