c027a459337e90911e9524c9350e36d1dff8368d
mHiluxS <108304662+mHijuxS@users.noreply.github.com>
2023-10-09 19:51:31 -0300

    AddPingSweeper

---
+++ Powershell/PingSweeper.ps1
@@ -0,0 +1,12 @@
+﻿param($p1)
+if (!$p1){
+    echo "Ping Sweeper"
+    echo "Exemplo de Uso .\script.ps1 192.168.0"
+}else{
+    foreach ($ip in 1..254){
+    try{
+        $resp = ping -n 1 -w 1 "$p1.$ip" | Select-String "bytes=32"
+        $resp.Line.split(' ')[2] -replace ":",""
+}catch{}
+}
+}
